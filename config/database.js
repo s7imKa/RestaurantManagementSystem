428b5e811620c6a716de4174772c71bfc1ec0824
maKs7imKa <maksikmaksik2005@gmail.com>
2025-04-24 20:00:53 +0300

    ...

--- config/database.js
+++ config/database.js
@@ -3,7 +3,6 @@ const { Sequelize } = require('sequelize');
 
 // Налаштування підключення до бази даних MySQL
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false,
